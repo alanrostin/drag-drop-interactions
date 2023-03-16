2dad71c2793284b29b8663b058bebf2c840c8a32
alanrostin <alan.rostin@gmail.com>
2023-03-16 20:43:33 +0100

    add drag events

--- app.js
+++ app.js
@@ -1,15 +1,23 @@
 const card = document.querySelector('.playing-card')
-
 const squares = document.querySelectorAll('.square')
+const infoDisplay = document.querySelector('#info')
 
+card.addEventListener('drag', dragging)
 card.addEventListener('dragstart', dragStart)
 
 squares.forEach(square => {
     square.addEventListener('dragover', dragOver)
+    square.addEventListener('dragenter', dragEnter)
+    square.addEventListener('dragleave', dragLeave)
     square.addEventListener('drop', dragDrop)
+    square.addEventListener('dragend', dragEnd)
 })
 
 let beingDragged
+
+function dragging(e) {
+    infoDisplay.textContent = 'You are dragging ' + beingDragged.id
+}
 
 function dragStart(e) {
     beingDragged = e.target
@@ -19,7 +27,21 @@ function dragOver(e) {
     e.preventDefault()
 }
 
-function dragDrop(e) {
-    e.target.append(beingDragged)
+function dragEnter(e) {
+    e.target.classList.add('highlight')
 }
 
+function dragLeave(e) {
+    e.target.classList.remove('highlight')
+}
+
+function dragDrop(e) {
+    e.target.append(beingDragged)
+    e.target.classList.remove('target')
+}
+
+function dragEnd(e) {
+    e.target.classList.add('target')
+    setTimeout(() => e.target.classList.remove('target'), 100)
+    infoDisplay.textContent = ''
+}
